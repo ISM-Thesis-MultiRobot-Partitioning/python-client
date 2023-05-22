b141a9b60e28c760cec1185669ef3a0c7e86d4ef
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-05-22 15:42:37 +0200

    make get/post request clients

---
+++ post.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from datetime import datetime
+import os
+
+API_URL = os.environ.get('PARTITION_API_URL')
+if not API_URL:
+    print("PARTITION_API_URL environment variable not set.")
+    print("Cannot access the API as a result.")
+    print("Exiting.")
+    exit(1)
+
+mydata = {
+    'vertices': [
+        { 'x': 50.0, 'y': 100.0, 'z': 0.0 },
+        { 'x': 200.0, 'y': 30.0, 'z': 0.0 },
+        { 'x': 350.0, 'y': 120.0, 'z': 0.0 },
+    ],
+    'resolution': { k: 2**4 for k in 'xyz' },
+    'me': {'x': 100.0, 'y': 80.0, 'z': 0.0},
+    'others': [
+        {'x': 130.0, 'y': 50.0, 'z': 0.0},
+        {'x': 80.0, 'y': 80.0, 'z': 0.0},
+    ],
+}
+
+# mydata = {
+#     'vertices': [
+#         { 'x': 0.0, 'y': 0.0, 'z': 0.0 },
+#         { 'x': 1.0, 'y': 1.0, 'z': 0.0 },
+#         { 'x': 2.0, 'y': 0.0, 'z': 0.0 },
+#     ],
+#     # 'resolution': { 'x': 2.0, 'y': 2.0, 'z': 2.0 },
+#     'resolution': { 'x': 1.5, 'y': 1.5, 'z': 1.5 },
+#     'me': {'x': 1.5, 'y': 0.5, 'z': 0.0},
+#     'others': [
+#     ],
+# }
+
+start = datetime.now()
+
+print("Making query ...")
+r = requests.post(f'{API_URL}/PolygonToCellMap', json=mydata)
+
+print("Parsing JSON ... ({})".format(datetime.now() - start))
+jdata = json.loads(r.text)
+
+end = datetime.now()
+
+
+total = end - start
+print(r, 'Time elapsed: {}'.format(total))
+
+print("{} cells were processed.".format(len(jdata['cells'])))
+for d in jdata['cells'][:10]:
+    print(d)
